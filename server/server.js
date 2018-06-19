ee0f9a24f3a0a0292c5be56310ab5809a7271f2c
hemalhansda <hemal.hansda25@gmail.com>
2018-06-19 22:24:49 +0530

    Added POST /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());;
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var newTodo = new Todo({
+//   text: 'Go for a Date',
+// });
+
+// var otherTodo = new Todo({
+//   text: '       Edit this Video        '
+// });
+//
+//
+// // newTodo.save().then((doc) => {
+// //   console.log('Saved todo', doc)
+// // }, (e) => {
+// //   console.log('Unable to save Todo');
+// // });
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (err) => {
+//   console.log('Unable to get todo');
+// });
+// var {mongoose} = require('./db/mongoose');
+//
+//
+// var newUser = new User({
+//   email: 'hemal.hansda25@gmail.com'
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log(doc);
+// }, (err) => {
+//   console.log('Unable to add data: ', err);
+// });

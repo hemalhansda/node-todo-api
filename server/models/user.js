b9fc65c0a46baf175131b9c47fb3fe1c761f2c94
hemalhansda <hemal.hansda25@gmail.com>
2018-06-22 01:39:40 +0530

    hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -73,11 +73,6 @@ UserSchema.pre('save', function (next) {
   var user = this;
 
   if(user.isModified('password')){
-    // bcrypt.genSalt(10, (err, salt) => {
-    //   bcrypt.hash(password, salt, (err, hash) => {
-    //     console.log(hash);
-    //   });
-    // });
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
